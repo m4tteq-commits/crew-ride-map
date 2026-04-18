b7e7f3b873e61c7b3c95484af325aa1d7e50dd3a
m4tteq-commits <m4tteq@gmail.com>
2026-04-18 22:10:32 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.matteq.crewridemap',
   appName: 'Drive Together',
   webDir: 'dist',
-  server: {
-    url: 'https://27410146-9e85-4aed-81e1-92071c45bfd6.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
   plugins: {
     Geolocation: {
       permissions: ['location'],
